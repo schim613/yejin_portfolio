acd1102847df03dc4b5e1ef8d80726be8ea70e91
schim613 <uximin@naver.com>
2021-09-02 03:10:44 +0900

    포폴 사진 추가

--- index.ssg.php
+++ index.ssg.php
@@ -17,7 +17,11 @@
     <!-- 포트폴리오 -->
     <section class="section relative fp-auto-height-responsive" data-anchor="portfolio">
       <a href="#home"><img src="resource/img/btn_1_2.png" alt="상단버튼" class="section-item icon-up"></a>
-      <div><img src="resource/img/3-1.png" alt="스티커1" class="section-item pf-img-1"></div>
+      <div><img src="resource/img/3-1.png" alt="포트폴리오1" class="section-item pf-img-1"></div>
+      <div><img src="resource/img/3-2.png" alt="스티커1" class="section-item pf-img-2"></div>
+      <div><img src="resource/img/3-3.png" alt="스티커2" class="section-item pf-img-3"></div>
+      <div><img src="resource/img/3-4.png" alt="스티커3" class="section-item pf-img-4"></div>
+      <div><img src="resource/img/3-5.png" alt="스티커4" class="section-item pf-img-5"></div>
     </section>
 
     <!-- 연락 -->
@@ -25,8 +29,9 @@
       <a href="#home"><img src="resource/img/btn_1_2.png" alt="상단버튼" class="section-item icon-up"></a>
       <div><img src="resource/img/4-1.png" alt="연락처" class="section-item ct-img-1"></div>
       <div><img src="resource/img/4-2.png" alt="연락폼" class="section-item ct-img-2"></div>
-      <div><img src="resource/img/4-3.png" alt="스티커2" class="section-item ct-img-3"></div>
-      <div><img src="resource/img/4-4.png" alt="스티커3" class="section-item ct-img-4"></div>
+      <div><img src="resource/img/4-3.png" alt="스티커1" class="section-item ct-img-3"></div>
+      <div><img src="resource/img/4-4.png" alt="스티커2" class="section-item ct-img-4"></div>
+      <div><img src="resource/img/4-5.png" alt="스티커3" class="section-item ct-img-5"></div>
       <form target="_blank" action="https://formspree.io/f/mnqlozzw" onsubmit="sendEmailForm(this); return false;"
         method="POST">
         <div class="absolute ct-form-1">
