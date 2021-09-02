40ff0454f264a7b763c8a23553b64c01674942b3
schim613 <uximin@naver.com>
2021-09-02 14:04:11 +0900

    포폴 섹션 작업

--- index.ssg.php
+++ index.ssg.php
@@ -17,11 +17,16 @@
     <!-- 포트폴리오 -->
     <section class="section relative fp-auto-height-responsive" data-anchor="portfolio">
       <a href="#home"><img src="resource/img/btn_1_2.png" alt="상단버튼" class="section-item icon-up"></a>
-      <div><img src="resource/img/3-1.png" alt="포트폴리오1" class="section-item pf-img-1"></div>
-      <div><img src="resource/img/3-2.png" alt="스티커1" class="section-item pf-img-2"></div>
-      <div><img src="resource/img/3-3.png" alt="스티커2" class="section-item pf-img-3"></div>
+      <div><img src="resource/img/3-1.png" alt="포트폴리오1" class="pf-img-1"></div>
+      <!-- <div><img src="resource/img/3-2.png" alt="스티커1" class="section-item pf-img-2"></div>
+      <div><img src="resource/img/3-3.png" alt="스티커2" class="section-item pf-img-3"></div> -->
       <div><img src="resource/img/3-4.png" alt="스티커3" class="section-item pf-img-4"></div>
       <div><img src="resource/img/3-5.png" alt="스티커4" class="section-item pf-img-5"></div>
+      <div class="mousemove-effect-1-el inline-block pf-img-2" data-mousemove-effect1-hor-res="0.01" data-mousemove-effect1-ver-res="0.01">
+  <img class="block" src="resource/img/3-2.png" alt="">
+      <div class="mousemove-effect-1-el inline-block pf-img-3" data-mousemove-effect1-hor-res="0.01" data-mousemove-effect1-ver-res="0.01">
+  <img class="block" src="resource/img/3-3.png" alt="">
+</div>
     </section>
 
     <!-- 연락 -->
