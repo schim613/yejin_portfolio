78645cc9c83aacf17e64b587de18e0f14b00fb2a
schim613 <uximin@naver.com>
2021-10-27 21:43:33 +0900

    자기소개 수정

--- index.ssg.php
+++ index.ssg.php
@@ -17,11 +17,46 @@
     <!-- <div><img src="resource/img/main-title.png" alt="메인타이틀" class="section-item hm-img-1"></div> -->
   </section>
 
-  <!-- 자기소개 -->
+  <!-- 자기소개
   <section class="section relative fp-auto-height-responsive" data-anchor="about-me">
     <a class="cursor-pointer" href="#home"><img src="resource/img/btn_1_2.png" alt="상단버튼"
         class="section-item icon-up"></a>
     <div class="flex items-center justify-center"><img src="resource/img/2-1.png" alt="프로필" class="am-img-1"></div>
+  </section> -->
+
+  <!-- 자기소개 -->
+  <section class="section relative fp-auto-height-responsive" data-anchor="about-me">
+    <a class="cursor-pointer" href="#home"><img src="resource/img/btn_1_2.png" alt="상단버튼"
+        class="section-item icon-up"></a>
+    <div style="width:618px; height: 458px;">
+      <img class="section-item am-img-1" src="resource/img/2-1.png" alt="프로필 사진">
+    </div>
+    <div class="flex items-center justify-center">
+      <div class="am-img-2" style="width:363px; height:388px;"></div>
+      <div class="am-img-3 flex flex-col items-center justify-center" style="width:363px; height:388px;">
+        <div class="my-10">
+          <a href="https://to2.kr/ckc" target="_blank" class="cursor-pointer flex flex-col items-center justify-center">
+            <img src="resource/img/icon-youtube.png" alt="">
+            <div class="mt-2">공부 영상</div>
+          </a>
+        </div>
+        <div class="flex">
+          <div class="mr-10">
+            <a href="https://to2.kr/ckb" target="_blank" class="cursor-pointer flex flex-col items-center justify-center">
+              <img src="resource/img/icon-codepen.png" alt="">
+              <div class="mt-2">공부 위키</div>
+            </a>
+          </div>
+          <div>
+            <a href="https://to2.kr/cwn" target="_blank" class="cursor-pointer flex flex-col items-center justify-center">
+              <img src="resource/img/icon-codepen.png" alt="">
+              <div class="mt-2">모작 위키</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="am-img-4" style="width:363px; height:388px;"></div>
+    </div>
   </section>
 
   <!-- 포트폴리오 -->
@@ -48,9 +83,9 @@
               <span>JYP 엔터테인먼트 리디자인</span>
               <img src="resource/img/pf_img1.png" alt="">
             </a>
-              <div class="mt-3">▶ 기획 100%, 코딩 100%</div>
-              <div class="mt-2">php 이용하여 메인페이지와 서브페이지의 상·하단바의 코드 중복을 최소화하여 효율적으로 만들었습니다.</div>
-              <div></div>
+            <div class="mt-3">▶ 기획 100%, 코딩 100%</div>
+            <div class="mt-2">php 이용하여 메인페이지와 서브페이지의 상·하단바의 코드 중복을 최소화하여 효율적으로 만들었습니다.</div>
+            <div></div>
           </div>
         </div>
         <div class="pf-content pf-2">
@@ -77,10 +112,11 @@
               <span>일룸 UI/UX 프로세스 페이지</span>
               <img src="resource/img/pf_img4.png" alt="">
             </a>
-              <div class="mt-3">▶ 팀 프로젝트</div>
-              <div class="my-2">타경쟁업체와 달리 기존 앱이 없어 온라인 구매에 어려움을 겪고 있는 기존 고객들에게 실용적이며 편리한 어플을 제공합니다. 또한 새롭게 유입하는 고객들에게 사용성을 높이는 편리한 UI/UX를 목표로 프로젝트를 진행했습니다.</div>
-              <a class="underline cursor-pointer" href="https://to2.kr/df7" target="_blank">▷ 상세페이지</a>
-              <a class="underline cursor-pointer" href="https://to2.kr/da1" target="_blank">▷ 기획 PPT</a>
+            <div class="mt-3">▶ 팀 프로젝트</div>
+            <div class="my-2">타경쟁업체와 달리 기존 앱이 없어 온라인 구매에 어려움을 겪고 있는 기존 고객들에게 실용적이며 편리한 어플을 제공합니다. 또한 새롭게 유입하는 고객들에게 사용성을
+              높이는 편리한 UI/UX를 목표로 프로젝트를 진행했습니다.</div>
+            <a class="underline cursor-pointer" href="https://to2.kr/df7" target="_blank">▷ 상세페이지</a>
+            <a class="underline cursor-pointer" href="https://to2.kr/da1" target="_blank">▷ 기획 PPT</a>
           </div>
         </div>
       </div>
